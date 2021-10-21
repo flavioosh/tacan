59ad606c64e6612cd4112787373e81e578daace2
Szymon Libudzki <flavioosh@gmail.com>
2021-10-22 00:07:30 +0100

    feat: Add ts paths config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -10,5 +11,13 @@ export default {
   ],
   kit: {
     adapter: adapter(),
+    vite: {
+      resolve: {
+        alias: {
+          $components: resolve('src/components'),
+          $types: resolve('src/types'),
+        },
+      },
+    },
   },
 };
